b60c9978a9306d86fc2a225dea3340954acd57b2
jeongminhong <azurestefan@gmail.com>
2021-02-16 23:49:26 +0900

    change to seasons

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,8 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-    return <div>Hi there!</div>;
+//functional component
+// const App = () => {
+//     window.navigator.geolocation.getCurrentPosition(
+//         (position) => console.log(position), //success callback: when we get back a result
+//         (err) => console.log(err)//failure callback 
+//     );
+
+//     return <div>Latitude: </div>;
+// };
+
+//class component
+class App extends React.Component {
+    constructor(props) {
+        super(props); //reference to the parent's(React.component) constructor function
+        
+        //THIS IS THE ONLY TIME we do direct assignment. Use setState
+        this.state = {lat: null};
+
+        window.navigator.geolocation.getCurrentPosition(
+            position => {
+                this.setState({ lat: position.coords.latitude}); //setState is a function that updates state.
+            }, //success callback: when we get back a result
+            err => console.log(err)//failure callback 
+        );
+    }
+    
+
+    //React says we have to define render!! Is going to be called frequently.
+    render() {
+        return <div>Latitude: {this.state.lat}</div>;
+    }
 }
 
 ReactDOM.render(<App/>, document.querySelector('#root'))
