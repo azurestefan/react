64797400fe4550a0fe53e427841d1832532ba5c3
jeongminhong <azurestefan@gmail.com>
2021-02-27 23:03:09 +0900

    translate component

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -2,48 +2,42 @@ import React, { useState } from 'react';
 import Accordion from './components/Accordion';
 import Dropdown from './components/Dropdown';
 import Search from './components/Search';
+import Translate from './components/Translate';
 
 const items = [
-    {
+  {
     title: 'What is React?',
-    content: 'React is a front end js framework'
-    },
-    {
-        title: 'Why use React?',
-        content: 'React is a great'
-    },
-    {
-        title: 'React?',
-        content: 'Use React by creating components'
-    },
+    content: 'React is a front end js framework',
+  },
+  {
+    title: 'Why use React?',
+    content: 'React is a great',
+  },
+  {
+    title: 'React?',
+    content: 'Use React by creating components',
+  },
 ];
 
 const options = [
-    {
-        label: 'The Color Red',
-        value: 'red'
-    },
-    {
-        label: 'The Color Green',
-        value: 'green'
-    },
-    {
-        label: 'The Color Blue',
-        value: 'blue'
-    }
+  {
+    label: 'The Color Red',
+    value: 'red',
+  },
+  {
+    label: 'The Color Green',
+    value: 'green',
+  },
+  {
+    label: 'The Color Blue',
+    value: 'blue',
+  },
 ];
 
 export default () => {
-
-    const [selected, setSelected] = useState(options[0]);
-
-    return (
-        <div>
-            <Dropdown 
-            selected={selected} 
-            onSelectedChange={setSelected}
-            options={options} 
-            />
-        </div>
-    );
+  return (
+    <div>
+      <Translate />
+    </div>
+  );
 };
--- widgets/src/components/Dropdown.js
+++ widgets/src/components/Dropdown.js
@@ -2,24 +2,26 @@ import React, { useState, useEffect, useRef } from 'react';
 
 const Dropdown = ({ label, options, selected, onSelectedChange }) => {
   const [open, setOpen] = useState(false);
-  const ref = useRef();
+  const ref = useRef(); //allows direct reference to a DOM element
 
   useEffect(() => {
-    //clicking outside the dropdown will close the dropdown
+    // useref targets dropdown
     const onBodyClick = (event) => {
-      if (ref.current.contains(event.target)) {
-        return;
+      if (ref.current && ref.current.contains(event.target)) {
+        //checks if the target DOM element is contained inside ref current component.
+        return; //will return early if it is.
       }
 
-      setOpen(false);
+      setOpen(false); //clicking outside the dropdown will close the dropdown.
     };
 
-    document.body.addEventListener('click', onBodyClick);
+    document.body.addEventListener('click', onBodyClick); //callback to the body event listener
 
+    //cleanup function that removes the eventlistener when the dropdown component is removed
     return () => {
-      document.body.removeEventListener('click', onBodyClick);
+      document.body.removeEventListener('click', onBodyClick); //remove the callback function onBodyClick
     };
-  }, []);
+  }, []); // only runs one time.
 
   //Makes current option not shown on the choice list.
   const renderedOptions = options.map((option) => {
@@ -28,29 +30,20 @@ const Dropdown = ({ label, options, selected, onSelectedChange }) => {
     }
 
     return (
-      <div
-        key={option.value}
-        className="item"
-        onClick={() => onSelectedChange(option)}
-      >
+      <div key={option.value} className='item' onClick={() => onSelectedChange(option)}>
         {option.label}
       </div>
     );
   });
 
   return (
-    <div ref={ref} className="ui form">
-      <div className="field">
-        <label className="label">{label}</label>
-        <div
-          onClick={() => setOpen(!open)}
-          className={`ui selection dropdown ${open ? 'visible active' : ''}`}
-        >
-          <i className="dropdown icon"></i>
-          <div className="text">{selected.label}</div>
-          <div className={`menu ${open ? 'visible transition' : ''}`}>
-            {renderedOptions}
-          </div>
+    <div ref={ref} className='ui form'>
+      <div className='field'>
+        <label className='label'>{label}</label>
+        <div onClick={() => setOpen(!open)} className={`ui selection dropdown ${open ? 'visible active' : ''}`}>
+          <i className='dropdown icon'></i>
+          <div className='text'>{selected.label}</div>
+          <div className={`menu ${open ? 'visible transition' : ''}`}>{renderedOptions}</div>
         </div>
       </div>
     </div>
